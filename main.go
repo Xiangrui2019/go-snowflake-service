8d258e42f4c756efbfe3a2df4c8b5876158a0d2a
Xiangrui2019 <xiangrui@aiursoft.com>
2019-12-01 16:28:30 +0800

    use server starter config

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
 	_ "go-snowflake-service/cache"
+	"go-snowflake-service/conf"
 	_ "go-snowflake-service/conf"
-	"go-snowflake-service/snowflake"
+	"go-snowflake-service/pb"
+	"go-snowflake-service/services"
 	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-/**
 func main() {
 	listen, err := net.Listen("tcp", conf.ConfigObject.Addr)
 
@@ -26,35 +30,4 @@ func main() {
 	if err := server.Serve(listen); err != nil {
 		log.Fatal(err)
 	}
-}
-**/
-
-func main() {
-	ch := make(chan snowflake.ID)
-	go func() {
-		count := 2000
-		// 并发 count 个 goroutine 进行 snowflake ID 生成
-		for i := 0; i < count; i++ {
-			go func() {
-				id, _ := workers.GenerateId(1)
-				log.Println("1", id)
-				ch <- id
-			}()
-		}
-	}()
-
-	m := make(map[snowflake.ID]int)
-	for i := 0; i < 2000; i++ {
-		id := <-ch
-		// 如果 map 中存在为 id 的 key, 说明生成的 snowflake ID 有重复
-		_, ok := m[id]
-		if ok {
-			fmt.Printf("ID is not unique!\n")
-			return
-		}
-		// 将 id 作为 key 存入 map
-		m[id] = i
-	}
-	// 成功生成 snowflake ID
-	fmt.Println("All  snowflake ID generate successed!\n")
 }
